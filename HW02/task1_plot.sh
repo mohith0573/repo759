eda8dde13fa444760e6789e4baf59d13608610b9
MOHITH THATIKONDA <mthatikonda@linux-2632.cae.wisc.edu>
2026-02-05 16:49:23 -0600

    modified

--- HW02/task1_plot.sh
+++ HW02/task1_plot.sh
@@ -3,10 +3,11 @@
 #SBATCH -J Task1_Plot
 #SBATCH --partition=instruction
 #SBATCH -o task1_plot.out -e task1_plot.err
+#SBATCH --mem=16G
 module load gcc/12.2.0
 for i in {10..30}
 do
     n=$((2**i))
     ./task1 $n >> task1_times.txt
 done
-python plot_task1.py
+python task1_plot.py
