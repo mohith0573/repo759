5cf414d229be19f1662e7b4bc2c7446be7900c1a
MOHITH THATIKONDA <mthatikonda@linux-2632.cae.wisc.edu>
2026-02-05 17:08:15 -0600

    modified

--- HW02/task1_plot.sh
+++ HW02/task1_plot.sh
@@ -5,9 +5,11 @@
 #SBATCH -o task1_plot.out -e task1_plot.err
 #SBATCH --mem=16G
 module load gcc/14.3.0
+source ~/myenv/bin/activate
 for i in {10..30}
 do
     n=$((2**i))
     ./task1 $n >> task1_times.txt
 done
 python task1_plot.py
+deactivate
