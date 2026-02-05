2521f4ed185c7dc812bae335e5e1cfcbc373575f
MOHITH THATIKONDA <mthatikonda@linux-2629.cae.wisc.edu>
2026-02-05 14:00:13 -0600

    task3 modified

--- HW02/matmul.cpp
+++ HW02/matmul.cpp
@@ -1,36 +1,36 @@
 #include "matmul.h"
 
-// i j k
-void mmul1(const double* A, const double* B, double* C, int n) {
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            for (int k = 0; k < n; k++)
+// i, j, k
+void mmul1(const double* A, const double* B, double* C, const unsigned int n) {
+    for (unsigned int i = 0; i < n; i++)
+        for (unsigned int j = 0; j < n; j++)
+            for (unsigned int k = 0; k < n; k++)
                 C[i*n + j] += A[i*n + k] * B[k*n + j];
 }
 
-// i k j
-void mmul2(const double* A, const double* B, double* C, int n) {
-    for (int i = 0; i < n; i++)
-        for (int k = 0; k < n; k++)
-            for (int j = 0; j < n; j++)
+// i, k, j
+void mmul2(const double* A, const double* B, double* C, const unsigned int n) {
+    for (unsigned int i = 0; i < n; i++)
+        for (unsigned int k = 0; k < n; k++)
+            for (unsigned int j = 0; j < n; j++)
                 C[i*n + j] += A[i*n + k] * B[k*n + j];
 }
 
-// j k i
-void mmul3(const double* A, const double* B, double* C, int n) {
-    for (int j = 0; j < n; j++)
-        for (int k = 0; k < n; k++)
-            for (int i = 0; i < n; i++)
+// j, k, i
+void mmul3(const double* A, const double* B, double* C, const unsigned int n) {
+    for (unsigned int j = 0; j < n; j++)
+        for (unsigned int k = 0; k < n; k++)
+            for (unsigned int i = 0; i < n; i++)
                 C[i*n + j] += A[i*n + k] * B[k*n + j];
 }
 
-// vector version
+// vector A, B; raw pointer C
 void mmul4(const std::vector<double>& A,
            const std::vector<double>& B,
-           std::vector<double>& C,
-           int n) {
-    for (int i = 0; i < n; i++)
-        for (int j = 0; j < n; j++)
-            for (int k = 0; k < n; k++)
+           double* C,
+           const unsigned int n) {
+    for (unsigned int i = 0; i < n; i++)
+        for (unsigned int j = 0; j < n; j++)
+            for (unsigned int k = 0; k < n; k++)
                 C[i*n + j] += A[i*n + k] * B[k*n + j];
 }
--- HW02/task2.sh
+++ HW02/task2.sh
@@ -3,4 +3,4 @@
 #SBATCH -J Task2
 #SBATCH --partition=instruction
 #SBATCH -o task2.out -e task2.err
-./task2 6 3
+./task2 4 3
