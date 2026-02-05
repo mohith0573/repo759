8a04640070092f8a7694479747837ff9341fb405
MOHITH THATIKONDA <mthatikonda@linux-2632.cae.wisc.edu>
2026-02-05 17:12:58 -0600

    modified

--- HW02/task1_plot.py
+++ HW02/task1_plot.py
@@ -1,15 +1,15 @@
 import matplotlib.pyplot as plt
 
-n_values = [2**i for i in range(10, 31)]
+n_values = [2**i for i in range(10, 29)]
 times = []
 
 with open("task1_times.txt") as f:
-    lines = f.readlines()
-    for i in range(0, len(lines), 3):  # skip first/last elements
-        times.append(float(lines[i].strip()))
+    lines = [line.strip() for line in f if line.strip() != '']  # skip blanks
+    for i in range(0, len(lines), 3):
+        times.append(float(lines[i]))
 
 plt.figure(figsize=(8,6))
-plt.plot(n_values, times, marker='o')
+plt.plot(n_values, times, marker='o', linestyle='-')
 plt.xscale('log', base=2)
 plt.yscale('log')
 plt.xlabel("Array size n")
