bc03b3ada86705fa6055db12006200af61caec68
MOHITH THATIKONDA <mthatikonda@linux-2632.cae.wisc.edu>
2026-03-12 18:33:58 -0500

    corrected files

--- HW07/plot1.py
+++ HW07/plot1.py
@@ -1,29 +1,34 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# sizes for HW07 sweep
-n = np.array([2**i for i in range(10,21)])
-
+# HW07 sweep data
 thrust = np.loadtxt("times_thrust.txt")
 cub = np.loadtxt("times_cub.txt")
 
-# HW05 data
-hw05_256 = np.loadtxt("times256.txt")
-hw05_1024 = np.loadtxt("times1024.txt")
+n_hw07 = np.array([2**i for i in range(10,21)])
+
+# HW05 files contain alternating result/time
+hw05_256_raw = np.loadtxt("times256.txt")
+hw05_1024_raw = np.loadtxt("times1024.txt")
+
+# extract only timing values
+hw05_256 = hw05_256_raw[1::2]
+hw05_1024 = hw05_1024_raw[1::2]
 
 n_hw05 = np.array([2**i for i in range(10,21)])
 
 plt.figure()
 
-plt.loglog(n, thrust, 'o-', label="Thrust Reduction")
-plt.loglog(n, cub, 's-', label="CUB Reduction")
+plt.loglog(n_hw07, thrust, 'o-', label="Thrust")
+plt.loglog(n_hw07, cub, 's-', label="CUB")
 
-plt.loglog(n_hw05, hw05_256, '^-', label="HW05 Reduction (256 threads)")
-plt.loglog(n_hw05, hw05_1024, 'd-', label="HW05 Reduction (1024 threads)")
+plt.loglog(n_hw05, hw05_256, '^-', label="HW05 CUDA (256 threads)")
+plt.loglog(n_hw05, hw05_1024, 'd-', label="HW05 CUDA (1024 threads)")
 
 plt.xlabel("Input size (n)")
 plt.ylabel("Time (ms)")
 plt.title("Reduction Scaling Comparison")
+
 plt.legend()
 plt.grid(True)
 
--- HW07/task3.cpp
+++ HW07/task3.cpp
@@ -3,9 +3,9 @@
 
 long factorial(int n)
 {
-    long f=1;
+    long f = 1;
     for(int i=1;i<=n;i++)
-        f*=i;
+        f *= i;
     return f;
 }
 
@@ -17,9 +17,14 @@ int main()
     int id = omp_get_thread_num();
 
 #pragma omp single
-    std::cout<<"Number of threads: "<<omp_get_num_threads()<<std::endl;
+    {
+        std::cout << "Number of threads: " << omp_get_num_threads() << std::endl;
+    }
 
-    std::cout<<"I am thread No. "<<id<<std::endl;
+#pragma omp critical
+    {
+        std::cout << "I am thread No. " << id << std::endl;
+    }
 }
 
 #pragma omp parallel for num_threads(4)
@@ -28,7 +33,9 @@ for(int i=1;i<=8;i++)
     long f = factorial(i);
 
 #pragma omp critical
-    std::cout<<i<<"!="<<f<<std::endl;
+    {
+        std::cout << i << "!=" << f << std::endl;
+    }
 }
 
 }
